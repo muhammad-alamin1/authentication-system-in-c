4e7c95cbd06b7e22babec69e3518ddba9d43e985
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2024-01-31 22:12:39 +0600

    Authentication System Console App

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+    Project Title: Authentication System in C
+    Language     : C
+    Github Rrepo :
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
